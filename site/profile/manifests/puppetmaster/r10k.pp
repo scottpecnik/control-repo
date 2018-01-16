1ba000c067bb0775cfa389e12c363b99abf55fd8
Scott Pecnik <contact@scottpecnik.com>
2018-01-16 14:38:21 -0500

    Adding in logic to create r10k directory

--- site/profile/manifests/puppetmaster/r10k.pp
+++ site/profile/manifests/puppetmaster/r10k.pp
@@ -4,10 +4,17 @@ class profile::puppetmaster::r10k {
 
   $control_repo_remote = lookup('puppetmaster::r10k::control_repo_remote')
 
+  file { '/etc/puppetlabs/r10k':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+  }
+
   file { '/etc/puppetlabs/r10k/r10k.yaml':
     ensure  => present,
     content => template('profile/puppetmaster/r10k.erb'),
     owner   => 'root',
     group   => 'root',
+    require => File['/etc/puppetlabs/r10k'],
   }
 }
